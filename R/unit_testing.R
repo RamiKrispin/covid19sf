11df0460b3fdfa00da9eba47892e59972ca60d38
RamiKrispin <ramkrisp@umich.edu>
2020-09-06 16:28:27 -0700

    add unit testing

---
+++ R/unit_testing.R
@@ -0,0 +1,16 @@
+test_that(desc = "Test the data dimension", {
+  expect_equal(base::nrow(covid19sf_summary) > 537, TRUE)
+  expect_equal(base::ncol(covid19sf_summary) == 5, TRUE)
+})
+
+
+test_that(desc = "Test dates/times variable", {
+  expect_equal(base::class(covid19sf_summary$specimen_collection_date) == "Date", TRUE)
+  expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_summary$last_updated)), TRUE)
+  expect_equal(base::min(covid19sf_summary$specimen_collection_date) == as.Date("2020-03-03"), TRUE)
+})
+
+
+test_that(desc = "Missing values", {
+  expect_equal(base::any(base::is.na(covid19sf_summary$case_count)), FALSE)
+})
