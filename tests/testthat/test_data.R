e59e337f5baa5764950c85ac5929bbbbd31ed1f5
RamiKrispin <ramkrisp@umich.edu>
2020-09-08 02:36:15 -0700

    add unit tests

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -5,6 +5,9 @@ test_that(desc = "Test the data dimension", {
   expect_equal(base::nrow(covid19sf_hospital) > 667, TRUE)
   expect_equal(base::ncol(covid19sf_hospital) == 5, TRUE)
 
+  expect_equal(base::nrow(covid19sf_tests) > 190, TRUE)
+  expect_equal(base::ncol(covid19sf_tests) == 7, TRUE)
+
 })
 
 
@@ -16,6 +19,10 @@ test_that(desc = "Test dates/times variable", {
   expect_equal(base::class(covid19sf_hospital$reportdate) == "Date", TRUE)
   expect_equal(base::min(covid19sf_hospital$reportdate) == as.Date("2020-03-23"), TRUE)
 
+  expect_equal(base::class(covid19sf_tests$specimen_collection_date) == "Date", TRUE)
+  expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_tests$last_updated)), TRUE)
+  expect_equal(base::min(covid19sf_tests$specimen_collection_date) == as.Date("2020-02-28"), TRUE)
+
 })
 
 
@@ -23,4 +30,10 @@ test_that(desc = "Missing values", {
   expect_equal(base::any(base::is.na(covid19sf_summary$case_count)), FALSE)
 
   expect_equal(base::any(base::is.na(covid19sf_hospital$patientcount)), FALSE)
+
+  expect_equal(base::any(base::is.na(covid19sf_tests$tests)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_tests$pos)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_tests$pct)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_tests$neg)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_tests$indeterminate)), FALSE)
 })
