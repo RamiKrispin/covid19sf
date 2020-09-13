60596a50a7ffcfd8986ddf6937515e27037fcf9c
RamiKrispin <ramkrisp@umich.edu>
2020-09-13 11:45:50 -0700

    update the tests

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -2,7 +2,7 @@ test_that(desc = "Test the data dimension", {
   expect_equal(base::nrow(covid19sf_summary) > 537, TRUE)
   expect_equal(base::ncol(covid19sf_summary) == 5, TRUE)
 
-  expect_equal(base::nrow(covid19sf_hospital) > 667, TRUE)
+  expect_equal(base::nrow(covid19sf_hospital) > 1230, TRUE)
   expect_equal(base::ncol(covid19sf_hospital) == 5, TRUE)
 
   expect_equal(base::nrow(covid19sf_tests) > 190, TRUE)
@@ -19,8 +19,8 @@ test_that(desc = "Test dates/times variable", {
   expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_summary$last_updated)), TRUE)
   expect_equal(base::min(covid19sf_summary$specimen_collection_date) == as.Date("2020-03-03"), TRUE)
 
-  expect_equal(base::class(covid19sf_hospital$reportdate) == "Date", TRUE)
-  expect_equal(base::min(covid19sf_hospital$reportdate) == as.Date("2020-03-23"), TRUE)
+  expect_equal(base::class(covid19sf_hospital$date) == "Date", TRUE)
+  expect_equal(base::min(covid19sf_hospital$date) == as.Date("2020-04-01"), TRUE)
 
   expect_equal(base::class(covid19sf_tests$specimen_collection_date) == "Date", TRUE)
   expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_tests$last_updated)), TRUE)
