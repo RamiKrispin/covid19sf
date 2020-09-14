2aa4b97278d71e15b633fe8c633e677d52d73e69
RamiKrispin <ramkrisp@umich.edu>
2020-09-13 21:19:57 -0700

    update the tests

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -11,6 +11,12 @@ test_that(desc = "Test the data dimension", {
   expect_equal(base::nrow(covid19sf_demo) > 1507, TRUE)
   expect_equal(base::ncol(covid19sf_demo) == 5, TRUE)
 
+  expect_equal(base::nrow(covid19sf_age) > 1430, TRUE)
+  expect_equal(base::ncol(covid19sf_age) == 5, TRUE)
+
+  expect_equal(base::nrow(covid19sf_gender) > 617, TRUE)
+  expect_equal(base::ncol(covid19sf_gender) == 5, TRUE)
+
 })
 
 
@@ -30,6 +36,14 @@ test_that(desc = "Test dates/times variable", {
   expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_demo$last_updated)), TRUE)
   expect_equal(base::min(covid19sf_demo$specimen_collection_date) == as.Date("2020-03-05"), TRUE)
 
+  expect_equal(base::class(covid19sf_age$specimen_collection_date) == "Date", TRUE)
+  expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_age$last_updated)), TRUE)
+  expect_equal(base::min(covid19sf_age$specimen_collection_date) == as.Date("2020-03-07"), TRUE)
+
+  expect_equal(base::class(covid19sf_gender$specimen_collection_date) == "Date", TRUE)
+  expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_gender$last_updated)), TRUE)
+  expect_equal(base::min(covid19sf_gender$specimen_collection_date) == as.Date("2020-03-07"), TRUE)
+
 })
 
 
@@ -46,4 +60,11 @@ test_that(desc = "Missing values", {
 
   expect_equal(base::any(base::is.na(covid19sf_demo$new_confirmed_cases)), FALSE)
   expect_equal(base::any(base::is.na(covid19sf_demo$cumulative_confirmed_cases)), FALSE)
+
+  expect_equal(base::any(base::is.na(covid19sf_age$new_confirmed_cases)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_age$cumulative_confirmed_cases)), FALSE)
+
+  expect_equal(base::any(base::is.na(covid19sf_gender$new_confirmed_cases)), FALSE)
+  expect_equal(base::any(base::is.na(covid19sf_gender$cumulative_confirmed_cases)), FALSE)
+
 })
