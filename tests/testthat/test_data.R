885661c08798c7e26a97a6c374c00ef63303ab3f
RamiKrispin <ramkrisp@umich.edu>
2020-09-14 01:07:46 -0700

    update the unit testing

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -20,9 +20,20 @@ test_that(desc = "Test the data dimension", {
   expect_equal(base::nrow(covid19sf_geo) == 264, TRUE)
   expect_equal(base::ncol(covid19sf_geo) == 8, TRUE)
 
-  expect_equal(base::nrow(covid19sf_homeless) > 165, TRUE)
+  expect_equal(base::nrow(covid19sf_homeless) > 164, TRUE)
   expect_equal(base::ncol(covid19sf_homeless) == 5, TRUE)
 
+  expect_equal(base::nrow(covid19sf_hospital) > 1230, TRUE)
+  expect_equal(base::ncol(covid19sf_hospital) == 5, TRUE)
+
+  expect_equal(base::nrow(covid19sf_hospitalizations) > 690, TRUE)
+  expect_equal(base::ncol(covid19sf_hospitalizations) == 5, TRUE)
+
+  expect_equal(base::nrow(covid19sf_housing) >= 43, TRUE)
+  expect_equal(base::ncol(covid19sf_housing) == 8, TRUE)
+
+  expect_equal(base::nrow(covid19sf_test_loc) >= 31, TRUE)
+  expect_equal(base::ncol(covid19sf_test_loc) == 17, TRUE)
 
 })
 
@@ -58,6 +69,14 @@ test_that(desc = "Test dates/times variable", {
   expect_equal(base::all(c("POSIXct", "POSIXt") %in% base::class(covid19sf_homeless$last_updated)), TRUE)
   expect_equal(base::min(covid19sf_homeless$specimen_collection_date) == as.Date("2020-03-31"), TRUE)
 
+  expect_equal(base::class(covid19sf_hospital$date) == "Date", TRUE)
+  expect_equal(base::min(covid19sf_hospital$date) == as.Date("2020-04-01"), TRUE)
+
+  expect_equal(base::class(covid19sf_hospitalizations$reportdate) == "Date", TRUE)
+  expect_equal(base::min(covid19sf_hospitalizations$reportdate) == as.Date("2020-03-23"), TRUE)
+
+  expect_equal(base::class(covid19sf_housing$date_updated) == "Date", TRUE)
+
 })
 
 
@@ -85,5 +104,26 @@ test_that(desc = "Missing values", {
 
   expect_equal(base::any(base::is.na(covid19sf_homeless$new_confirmed_cases)), FALSE)
   expect_equal(base::any(base::is.na(covid19sf_homeless$cumulative_confirmed_cases)), FALSE)
+
+  expect_equal(base::any(base::is.na(covid19sf_hospital$count)), FALSE)
+
+  expect_equal(base::any(base::is.na(covid19sf_hospitalizations$patientcount)), FALSE)
+
+})
+
+
+test_that(desc = "Duplications", {
+  expect_equal(base::any(base::duplicated(covid19sf_age)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_demo)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_gender)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_geo)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_homeless)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_hospital)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_hospitalizations)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_housing)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_summary)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_tests)), FALSE)
+  expect_equal(base::any(base::duplicated(covid19sf_test_loc)), FALSE)
+
 
 })
