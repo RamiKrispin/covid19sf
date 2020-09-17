ddc22ca6148a524b17a27d925336d49b8c041ada
RamiKrispin <ramkrisp@umich.edu>
2020-09-16 21:41:29 -0700

    update the docker file

--- docker/dev/install_packages.R
+++ docker/dev/install_packages.R
@@ -23,19 +23,35 @@ pkg_list <- c("dplyr",
               "viridisLite")
 
 
-install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
+
+
+for(i in pkg_list){
+  cat("\n")
+  cat(paste0("\033[0;", 42, "m","Installing ", i,"\033[0m","\n"))
+  install.packages(pkgs = i, repos = "https://cran.rstudio.com/", quiet = TRUE)
+  if(i %in% rownames(installed.packages())){
+    cat(paste0("\033[0;", 42, "m",i, " installed successfully ", "\033[0m","\n"))
+  } else {
+    cat(paste0("\033[0;", 41, "m", "failed to installed", i, "\033[0m","\n"))
+    stop()
+  }
+}
 
 # Installing the "rnaturalearthhires" package
-install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+# install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+
+remotes::install_github("ropensci/rnaturalearthhires")
 
 fail <- FALSE
 
 for(i in c(pkg_list, "rnaturalearthhires")){
 
   if(i %in% rownames(installed.packages())){
-    cat(i, "...OK\n")
+    cat(i, "...")
+    cat(paste0("\033[0;", 42, "m","OK","\033[0m","\n"))
   } else {
-    cat(i, "...Fail\n")
+    cat(i, "...")
+    cat(paste0("\033[0;", 41, "m","Fail","\033[0m","\n"))
     fail <- TRUE
   }
 
