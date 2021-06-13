1c32e48b9316e5f72dd2fb04719a6b70bb25f304
RamiKrispin <ramkrisp@umich.edu>
2021-06-13 10:31:38 -0700

    add vaccine data

--- data_raw/init_data_pull.R
+++ data_raw/init_data_pull.R
@@ -181,3 +181,10 @@ nrow(covid19sf_homeless)
 
 usethis::use_data(covid19sf_homeless, overwrite = TRUE)
 write.csv(covid19sf_homeless, "csv/covid19sf_homeless.csv", row.names = FALSE)
+
+
+# Vaccine demograpic summary
+covid19vaccine_demo <- read.csv("https://data.sfgov.org/resource/wv2h-rqwk.csv?$limit=1000", stringsAsFactors = FALSE)
+
+usethis::use_data(covid19vaccine_demo, overwrite = TRUE)
+write.csv(covid19vaccine_demo, "csv/covid19vaccine_demo.csv", row.names = FALSE)
